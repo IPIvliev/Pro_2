14f48ffd3f59bdc8aa54135a9c8b3aca6b4b77d0
IPIvliev <cccp1945@mail.ru>
2022-02-26 18:46:15 +0300

    Remove moduls for Dental

--- Screens/ManageWindow/manage_window.py
+++ Screens/ManageWindow/manage_window.py
@@ -1,13 +1,16 @@
 from kivy.uix.screenmanager import Screen
 from Moduls.vat_stepper import VatMotor
+from Moduls.led import LED
 
 class ManageWindow(Screen):
 	
 	def clean_display(self):
 		if self.clean_toggle.state == 'down':
 			self.clean_toggle.text = 'Включено'
+			LED.turn_led_on()
 		elif self.clean_toggle.state == 'normal':
 			self.clean_toggle.text = 'Выключено'
+			LED.turn_led_off()
 
 	def round_vat(self):
 		if self.vat_round_toggle.state == 'down':
