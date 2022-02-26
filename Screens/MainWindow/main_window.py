42954d731f5b6f3f90aa5e225216b1d133a10f5d
IPIvliev <cccp1945@mail.ru>
2022-02-26 18:49:20 +0300

    Remove moduls for Dental

--- Screens/MainWindow/main_window.py
+++ Screens/MainWindow/main_window.py
@@ -1,12 +1,8 @@
 from kivy.uix.screenmanager import Screen
-from Moduls.open_door import OpenDoor
 from Moduls.light import Light
 
 
 class MainWindow(Screen):
-
-	def open_door(self):
-		OpenDoor.open_door(self.parent)
 
 	def turn_light(self):
 		Light.light(self.parent)
