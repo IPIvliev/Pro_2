b4d31592935ab8a0768a08bda23ab9cf568d215a
IPIvliev <cccp1945@mail.ru>
2022-02-28 17:47:02 +0300

    Add pump motor

--- Moduls/pump_motor.py
+++ Moduls/pump_motor.py
@@ -10,7 +10,7 @@ AI2 = 19
 Standby = 6
 
 Freq = 50
-
+PMotor = rpi_dc_lib.TB6612FNGDc(AI1, AI2, PWA, Freq, False, "motor_one")
 class PumpMotor():
 
 	def pump_go(direction, delay, ti):
@@ -18,7 +18,7 @@ class PumpMotor():
 			try:
 				# Declare an named instance of class pass a name and motor type
 				rpi_dc_lib.TB6612FNGDc.standby(Standby, True)
-				PMotor = rpi_dc_lib.TB6612FNGDc(AI1, AI2, PWA, Freq, False, "motor_one")
+				
 				PMotor.forward(99) # Направление (direction) может быть forward или backward
 				time.sleep(0.5)
 			except Exception as error:
@@ -28,6 +28,7 @@ class PumpMotor():
 				PMotor.cleanup(False)
 				rpi_dc_lib.TB6612FNGDc.standby(Standby, False)
 		else:
+			rpi_dc_lib.TB6612FNGDc.standby(Standby, True)
 			PMotor.backward(80)
 			print("Motor go back")
 		#time.sleep(5)
