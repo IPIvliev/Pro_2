c0abefa18384abe210834a6b955e2ec741dfa732
IPIvliev <cccp1945@mail.ru>
2022-02-28 14:59:38 +0300

    Add pump motor

--- Screens/VatManageWindow/vat_manage_window.py
+++ Screens/VatManageWindow/vat_manage_window.py
@@ -6,7 +6,8 @@ from functools import partial
 class VatManageWindow(Screen):
 	def pour_in(self):
 		print("Clock motor GO")
-		self.event = Clock.schedule_interval(partial(PumpMotor.pump_go, 'forward', 1), 1)
+		Clock.schedule_once(partial(PumpMotor.pump_go, 'forward', 1), 1)
+		#self.event = Clock.schedule_interval(partial(PumpMotor.pump_go, 'forward', 1), 1)
 		#PumpMotor.pump_go('forward', 0.1)
 
 	def pour_out(self):
