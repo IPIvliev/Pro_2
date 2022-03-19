4f9bee694dfcb5c0e8e5f40792291f65368a41a2
IPIvliev <cccp1945@mail.ru>
2022-03-19 15:44:10 +0300

    Improve pump stepper

--- pump1.py
+++ pump1.py
@@ -12,15 +12,18 @@ Standby = GlobalValues.PUMPSTANDBY
 if __name__ == '__main__':
     try:
         print("START")
+        time.sleep(0.1)
         GPIO.output(Standby, True)
+        time.sleep(0.1)
         print("motorone set")
         GPIO.output(AI1, True)
         GPIO.output(AI2, False)
         time.sleep(2)
         print("motor test")
         GPIO.output(PWA, True)
-        time.sleep(5)
+        time.sleep(20)
     finally:
+        print("Stop test")
         GPIO.output(Standby, False)
         GPIO.cleanup()
 
