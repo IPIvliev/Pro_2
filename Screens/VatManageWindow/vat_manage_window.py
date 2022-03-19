3c470ed907550f125c0fe9c903d60069a4cfc788
IPIvliev <cccp1945@mail.ru>
2022-03-19 17:32:59 +0300

    Add weight in Vat screen

--- Screens/VatManageWindow/vat_manage_window.py
+++ Screens/VatManageWindow/vat_manage_window.py
@@ -8,8 +8,8 @@ from Moduls.scale import Scale
 class VatManageWindow(Screen):
 	def on_enter(self):
 		self.ids.scale_value.text = "0.00"
-		scale_value = Clock.schedule_interval(partial(Scale.readCount()), 0.1)
-		self.ids.scale_value.text = str(scale_value)
+		Clock.schedule_interval(partial(VatManageWindow.start_scale), 0.1)
+		
 
 	def pour_in(self):
 		self.event = Clock.schedule_interval(partial(PumpMotor.pump_go, 'forward'), 0.002)
@@ -20,3 +20,9 @@ class VatManageWindow(Screen):
 	def pour_stop(self):
 		self.event.cancel()
 		self.event = Clock.schedule_once(partial(PumpMotor.pump_stop), 0.01)
+
+	def start_scale():
+		sample = Scale.readCount()
+		w = sample/106
+		self.ids.scale_value.text = str(w)
+		time.sleep(0.1)
