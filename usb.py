d731204228f44d89ba4d6d74dad9ba6da5edc2a2
IPIvliev <cccp1945@mail.ru>
2022-03-20 15:25:40 +0300

    Change scale for Z and Vat

---
+++ usb.py
@@ -0,0 +1,22 @@
+import os
+import pyudev
+from glob import glob
+ 
+def main():
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='usb')
+    c=0
+    for device in iter(monitor.poll, None):
+ 
+        if device.action == 'add' and c == 0:
+            
+            path = glob("/media/fox/*/")
+            print(path[0])
+            c = 1
+ 
+        if device.action == 'remove':
+            c = 0
+ 
+if __name__ == '__main__':
+    main()
