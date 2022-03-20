3fac0b110aa709565454d4979af7d7737719c507
IPIvliev <cccp1945@mail.ru>
2022-03-20 13:53:10 +0300

    Change scale for Z and Vat

--- Moduls/scale.py
+++ Moduls/scale.py
@@ -10,13 +10,18 @@ stop = False
 class Scale():
 
   def readCount():
+    time.sleep(0.01)
     GPIO.setup(SCK, GPIO.OUT)
     i=0
     Count=0
+    time.sleep(0.01)
     GPIO.setup(DT, GPIO.OUT)
     GPIO.output(DT,1)
+    time.sleep(0.01)
     GPIO.output(SCK,0)
+    time.sleep(0.01)
     GPIO.setup(DT, GPIO.IN)
+    time.sleep(0.01)
 
     while GPIO.input(DT) == 1:
         i=0
