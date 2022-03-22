c9f06621677fcfd51c723cfa4d98163f7e593aeb
IPIvliev <cccp1945@mail.ru>
2022-03-22 16:00:48 +0300

    Add asyncio to Scale in Vat manager screen

--- Screens/MainWindow/main_window.py
+++ Screens/MainWindow/main_window.py
@@ -1,8 +1,10 @@
 from kivy.uix.screenmanager import Screen
 from Moduls.light import Light
-
+import threading
 
 class MainWindow(Screen):
 
 	def turn_light(self):
 		Light.light(self.parent)
+		n_thread =  threading.active_count()
+		print(n_thread)
