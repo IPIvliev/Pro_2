48918d2efe2b3892cfabbf35c5aa7b3ca76d1e29
IPIvliev <cccp1945@mail.ru>
2022-03-22 15:46:32 +0300

    Add asyncio to Scale in Vat manager screen

--- Screens/VatManageWindow/vat_manage_window.py
+++ Screens/VatManageWindow/vat_manage_window.py
@@ -4,17 +4,24 @@ from kivy.clock import Clock
 from functools import partial
 from Moduls.scale import Scale
 import time
+import asyncio
 
 class VatManageWindow(Screen):
 	def on_enter(self):
 		scale = Scale.readCount()
 		self.ids.scale_value.text = "0.00"
 		global EVENT
-		EVENT = Clock.schedule_interval(partial(VatManageWindow.start_scale, self, scale), 1)
+		#EVENT = Clock.schedule_interval(partial(VatManageWindow.start_scale, self, scale), 1)
+		ioloop = asyncio.get_event_loop()
+		tasks = [
+			ioloop.create_task(VatManageWindow.start_scale(self, scale))
+		]
+		ioloop.run_until_complete(asyncio.wait(tasks))
+		ioloop.close()
 	
 	def on_pre_leave(self):
 		global EVENT
-		EVENT.cancel()
+		#EVENT.cancel()
 
 	def pour_in(self):
 		self.event = Clock.schedule_interval(partial(PumpMotor.pump_go, 'forward'), 0.002)
@@ -26,9 +33,14 @@ class VatManageWindow(Screen):
 		self.event.cancel()
 		self.event = Clock.schedule_once(partial(PumpMotor.pump_stop), 0.01)
 
-	def start_scale(self, scale, ti):
-		sample = Scale.readCount()
-		time.sleep(0.1)
-		w = (scale - sample)/106
-		self.ids.scale_value.text = str(round(w))
-		time.sleep(0.1)
+	async def start_scale(self, scale, ti):
+		i = 0
+		long = 10
+		while long < i:
+			sample = Scale.readCount()
+			time.sleep(0.1)
+			i++
+			w = (scale - sample)/106
+			self.ids.scale_value.text = str(round(w))
+			time.sleep(0.1)
+			await asyncio.sleep(1)
