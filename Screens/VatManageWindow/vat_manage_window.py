42b2c95c6f74797bd0b3cdaaeca6ef31f45f967e
IPIvliev <cccp1945@mail.ru>
2022-05-03 18:04:27 +0300

    Change pump schedule time

--- Screens/VatManageWindow/vat_manage_window.py
+++ Screens/VatManageWindow/vat_manage_window.py
@@ -4,14 +4,16 @@ from kivy.clock import Clock
 from functools import partial
 from Moduls.scale import Scale
 import time
-import asyncio
+import multiprocessing
 
 class VatManageWindow(Screen):
 	def on_enter(self):
 		scale = Scale.readCount()
 		self.ids.scale_value.text = "0.00"
 		global EVENT
-		EVENT = Clock.schedule_interval(partial(VatManageWindow.start_scale, self, scale), 1)
+		moving = multiprocessing.Process(target=VatManageWindow.start_scale, args=(self, scale))
+
+		# EVENT = Clock.schedule_interval(partial(VatManageWindow.start_scale, self, scale), 1)
 		# ioloop = asyncio.get_event_loop()
 		# tasks = [
 		# 	ioloop.create_task(VatManageWindow.start_scale(self, scale))
@@ -33,7 +35,7 @@ class VatManageWindow(Screen):
 		self.event.cancel()
 		self.event = Clock.schedule_once(partial(PumpMotor.pump_stop), 0.01)
 
-	def start_scale(self, scale, ti):
+	def start_scale(self, scale):
 		# i = 0
 		# long = 10
 		# while long < i:
