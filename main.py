4da077ca45ec8cb78fff2f635cc346730f3b043b
IPIvliev <cccp1945@mail.ru>
2022-05-03 14:28:59 +0300

    Change pump schedule time

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.uix.screenmanager import ScreenManager
 from Screens.MainWindow.main_window import MainWindow
 from Screens.ManageWindow.manage_window import ManageWindow
 from Screens.SettingsWindow.settings_window import SettingsWindow
-# from Screens.AboutWindow.about_window import AboutWindow
+from Screens.AboutWindow.about_window import AboutWindow
 from Screens.PrintWindow.print_window import PrintWindow
 from Screens.CalibrationWindow.calibration_window import CalibrationWindow
 from Screens.ZMovingWindow.z_moving_window import ZMovingWindow
