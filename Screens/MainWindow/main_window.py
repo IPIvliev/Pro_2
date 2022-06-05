84b6217e9a41f1c3e3ae9c8b8b1f54269a1bef52
IPIvliev <cccp1945@mail.ru>
2022-06-05 13:07:59 +0300

    Change color

--- Screens/MainWindow/main_window.py
+++ Screens/MainWindow/main_window.py
@@ -1,10 +1,4 @@
 from kivy.uix.screenmanager import Screen
-from Moduls.light import Light
-import threading
 
 class MainWindow(Screen):
-
-	def turn_light(self):
-		Light.light(self.parent)
-		n_thread =  threading.active_count()
-		print(n_thread)
+	pass
